78be2476e02827e4bdb6eca1605ab0f1acb70f5c
achakhansa24-web <achakhansa24@gmail.com>
2025-12-16 22:02:36 +0700

    Update komposer_103012400131.cpp

--- Tugas-Besar_IF4803_kelompok_13/komposer_103012400131.cpp
+++ Tugas-Besar_IF4803_kelompok_13/komposer_103012400131.cpp
@@ -36,6 +36,50 @@ void deleteAfterParent(ListParent &L, addressP prec, addressP &P){
     }
 }
 
+void findLeastProductiveParent(ListParent L){
+    if (L.first == nullptr){
+        cout << "List Komposer Kosong." << endl;
+        return;
+    }
+    addressP P = L.first;
+    int minMusic = -1;
+
+    while (P!= nullptr){
+        int jumMusik = 0;
+        addressC C = P->firstChild;
+
+        while (C != nullptr){
+            jumMusik++;
+            C = C->next;
+        }
+
+        if (minMusic == -1 || jumMusik < minMusic){
+            minMusic = jumMusik;
+        }
+
+        P = P->next;
+    }
+
+    cout << "komposer dengan jumlah musik paling sedikit ( " << minMusic << " lagu): " << endl;
+
+    P = L.first;
+    while (P != nullptr){
+        int jumMusik = 0;
+        addressC C = P->firstChild;
+
+        while (C != nullptr){
+            jumMusik++;
+            C = C->next;
+        }
+        if (jumMusik == minMusic){
+            cout << "- ID : " << P->infoK.ID << endl;
+            cout << " Nama : " << P->infoK.nama << endl;
+        }
+        P= P->next;
+    }
+}
+
+
 void viewParent(ListParent L){
   if (L.first == nullptr){
         cout << "Tidak ada komposer. \n";
